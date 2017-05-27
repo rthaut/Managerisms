9e9a0a217bb0b6c3520fa7b32ed52395a422a94a
rthaut <rthaut@gmail.com>
2017-05-26 22:04:51 -0600

    primary navigation tweaks

--- client/src/js/scripts.js
+++ client/src/js/scripts.js
@@ -1,3 +1,14 @@
 String.prototype.trail = function (length) {
   return this.length > length ? this.substring(0, length) + "..." : this;
 }
+
+// collapse navbar toggle when clicking nav item
+$(document).on('click', '.navbar-collapse.show', function (e) {
+  if ($(e.target).is('a') && ($(e.target).attr('class') != 'dropdown-toggle')) {
+    try {
+      $(this).collapse('hide');
+    } catch (e) {
+      void (e);
+    }
+  }
+});
